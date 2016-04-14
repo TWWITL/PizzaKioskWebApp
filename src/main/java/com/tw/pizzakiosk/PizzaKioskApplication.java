a4f3985e16c5e244012ba93c034b21aec7fb7739
Dé <rahul080327@gmail.com>
2016-04-14 19:05:12 +0530

    Rahul, Prachi | #000 | fix goof up for formatting in kiosk

--- src/main/java/com/tw/pizzakiosk/PizzaKioskApplication.java
+++ src/main/java/com/tw/pizzakiosk/PizzaKioskApplication.java
@@ -20,34 +20,34 @@ import java.util.Map;
 public class PizzaKioskApplication {
 
 
-        @RequestMapping("/")
-        public String home(Model model) {
-            model.addAttribute("sizes", DataSource.instance().getSizeMap().keySet());
-            model.addAttribute("crustTypes", DataSource.instance().getCrustsList());
-            model.addAttribute("toppings", DataSource.instance().getToppingMap().keySet());
-            return "OrderPizza";
-        }
-
-        @RequestMapping(value = "/orderPizza", method = RequestMethod.POST)
-        public String orderPizza(@ModelAttribute(value="orderPizzaForm") OrderPizzaForm orderPizzaForm, Model model) throws Exception {
-            Map<String, Size> sizeMap = DataSource.instance().getSizeMap();
-            List<Crust> crustsList = DataSource.instance().getCrustsList();
-            Map<String, Topping> toppingMap = DataSource.instance().getToppingMap();
-            model.addAttribute("sizes", sizeMap.keySet());
-            model.addAttribute("crustTypes", crustsList);
-            model.addAttribute("toppings", toppingMap.keySet());
-            try {
-                PizzaKiosk kiosk = PizzaKiosk.getInstance();
-                double price = kiosk.makePizzaWith(crustsList.get(0),orderPizzaForm.getSize(),orderPizzaForm.getToppings());
-                model.addAttribute("price", price);
-            }catch (Exception e) {
-                e.printStackTrace();
-                model.addAttribute("price", 0);
-            }
-            return "OrderPizza";
-        }
-
-        public static void main(String[] args) throws Exception {
-            SpringApplication.run(PizzaKioskApplication.class, args);
-        }
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("sizes", DataSource.instance().getSizeMap().keySet());
+        model.addAttribute("crustTypes", DataSource.instance().getCrustsList());
+        model.addAttribute("toppings", DataSource.instance().getToppingMap().keySet());
+        return "OrderPizza";
     }
+
+    @RequestMapping(value = "/orderPizza", method = RequestMethod.POST)
+    public String orderPizza(@ModelAttribute(value = "orderPizzaForm") OrderPizzaForm orderPizzaForm, Model model) throws Exception {
+        Map<String, Size> sizeMap = DataSource.instance().getSizeMap();
+        List<Crust> crustsList = DataSource.instance().getCrustsList();
+        Map<String, Topping> toppingMap = DataSource.instance().getToppingMap();
+        model.addAttribute("sizes", sizeMap.keySet());
+        model.addAttribute("crustTypes", crustsList);
+        model.addAttribute("toppings", toppingMap.keySet());
+        try {
+            PizzaKiosk kiosk = PizzaKiosk.getInstance();
+            double price = kiosk.makePizzaWith(crustsList.get(0), orderPizzaForm.getSize(), orderPizzaForm.getToppings());
+            model.addAttribute("price", price);
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("price", 0);
+        }
+        return "OrderPizza";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PizzaKioskApplication.class, args);
+    }
+}
